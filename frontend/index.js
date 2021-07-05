ca3c559f9989dabe0e9bc7f211b8e14255620a01
54x1 <five4x1@gmail.com>
2021-07-05 19:44:27 +1000

    up

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,8 @@
 const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
-const socket = io('https://degrassi-mountie-63053.herokuapp.com/');
-// const socket = io('http://127.0.0.1:8080/');
+
+const socket = io('https://sleepy-island-33889.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
