85a8fafbbb4be8f18ac2a0fff5312e3afe8af5e2
54x1 <five4x1@gmail.com>
2021-08-02 10:27:38 +1000

    Update utils.js

--- server/utils.js
+++ server/utils.js
@@ -12,17 +12,4 @@ function makeid(length) {
    }
    return result;
 
-}
-function getVal(){
-  $.getJSON("perks.json", function(data, perkOne, perkTwo){
-
-      var randIn = Math.floor(Math.random() * (data.perks.length));
-      var randIn2 = Math.floor(Math.random() * (data.perks.length));
-        var perkValue1 = data.perks[randIn].card;
-          var perkValue2 = data.perks[randIn2].card;
-          var perkOne = perkValue1;
-            var perkTwo = perkValue2;
-               return {perkOne, perkTwo};
-
-  });
-}
+} 
